52f13a656959b9cf3cb458ad0622cb874d5f529a
Aaryan <aaryan3619sharma@gmail.com>
2026-08-16 11:51:50 +0530

    some more collections/hashmap code practice

--- DSA-Collections/src/Main.java
+++ DSA-Collections/src/Main.java
@@ -1,12 +1,20 @@
 public static void main(String[] args) {
-    ArrayList<String> passedStudents = new ArrayList<>();
-    passedStudents.add("Alice");
-    passedStudents.add("Bob");
-    passedStudents.add("Charlie");
+//    ArrayList<String> passedStudents = new ArrayList<>();
+//    passedStudents.add("Alice");
+//    passedStudents.add("Bob");
+//    passedStudents.add("Charlie");
+//
+//    HashMap<Integer, String> StudentDirectory = new HashMap<>();
+//    StudentDirectory.put(101, "David");
+//    StudentDirectory.put(102, "Eve");
+//
+//    System.out.println(StudentDirectory.get(101));
+    HashMap<Integer, Integer> map1 = new HashMap<>();
+    int[] arr = {101, 102, 101, 103, 101, 102};
 
-    HashMap<Integer, String> StudentDirectory = new HashMap<>();
-    StudentDirectory.put(101, "David");
-    StudentDirectory.put(102, "Eve");
+    for (int i = 0; i < arr.length; i++){
+        map1.put(arr[i], map1.getOrDefault(arr[i], 0) + 1);
+    }
 
-    System.out.println(StudentDirectory.get(101));
+    System.out.println(map1);
 }
